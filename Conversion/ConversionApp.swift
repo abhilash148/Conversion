a86af7b3c1f5dcf3462e767a8540e99e75102407
abhilash gudavalli <saiabhilash148@gmail.com>
2022-09-18 23:35:27 +0530

    18-09-22: Conversion basic setup

---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Sai Abhilash Gudavalli on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
